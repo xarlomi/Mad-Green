0da3aa2d6f867181d5ecd24e7ebdf2cfa9629ab2
xarlomi <cportillogarrigues@gmail.com>
2021-03-07 23:19:36 +0100

    picture added

--- streamlit_cbd.py
+++ streamlit_cbd.py
@@ -1,19 +1,23 @@
 import streamlit as st
 from src.data_to_streamlit import *
-
 import pandas as pd
 import folium
 import codecs
 from streamlit_folium import folium_static
 import streamlit.components.v1 as components
+from PIL import Image
+import altair as alt
+imagen = Image.open("images/imagen_header.jpg")
+st.image(imagen)
+
 
 
 st.write("""
 # Mad-Green
-Comparador de precios de CBD Madrid
+**Tu comparador de precios de CBD en Madrid**
 """)
 
-mood = st.selectbox("## ¿Cómo te gustaría sentirte hoy 🚀?", mood_list())
+selected_mood = st.selectbox("¿Cómo te gustaría sentirte hoy 🚀?", mood_list())
 
-st.write(f"## Hay {count_cbd_mood(mood)} con ese {mood}"
-)
+
+st.write(f"### Hay {count_cbd_mood(selected_mood)} tipos de CBD que te harán sentirte {selected_mood}")
