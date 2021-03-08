8ddc09cbc4b534a6523b8647be15e5dae6ae1f81
xarlomi <cportillogarrigues@gmail.com>
2021-03-08 20:21:38 +0100

    query with df

--- src/data_to_streamlit.py
+++ src/data_to_streamlit.py
@@ -9,6 +9,7 @@ def carga_data():
     data = pd.read_csv("data/cbd_info2.csv")
     return data
 
+#Returns a list with all the moods that CBD makes you feel
 def mood_list():
     dat = carga_data()
     a = dat.Effects.unique()
@@ -22,7 +23,25 @@ def mood_list():
                 if x == "None" or x=="Dry" or x=="Mouth":
                     z.remove(x)
     return z
- 
+
+
+#This counts how many kind of CBDs exist that will make you feel in function of the choosen moood 
 def count_cbd_mood(mood):
     moods = db.cbd_info.count({"mood": {"$regex": f"{mood}"}})
     return moods
+
+#This function returns a bud shop in function of the mood desired to feel by user:
+def shop_mood(moods):
+    filt = {"mood":{"$regex":f"{moods}"}}
+    project = {"_id":0, "product":1}
+    result = db.cbd_info.find(filt, project)
+    results= list(result)
+    
+    lista_res = []
+    for i in results:
+        q = {"product":i["product"]}
+        pro = {"_id":0} 
+        if check_exists(q, "cbd_shops"):
+            lista_res.append(read_coll("cbd_shops",q,  pro))
+
+    return lista_res
